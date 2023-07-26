c201eefe5d889a04de7a6d047ceefc748e037600
charity <wa.nelimacharity@gmail.com>
2023-07-26 17:30:49 +0300

    polishing all about functions

--- app.js
+++ app.js
@@ -3,3 +3,18 @@
 // strict inequality operator (!==)
 // loose equality operator (==)
 // loose inequality operator (!=)
+// day 3
+// function declaration
+// function expression
+// calling a function
+function sayHello(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+  sayHello("John"); // Output: Hello, John!
+
+  function learn(subjects) {
+    console.log(`am learning, ${subjects}!`);
+  }
+  
+  learn("Swahili")
