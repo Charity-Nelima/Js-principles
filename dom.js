5160927b451d50fb76c7c80ba0c1e43d7f7b80dc
charity <wa.nelimacharity@gmail.com>
2023-07-30 22:55:28 +0300

    DOM structure

---
+++ dom.js
@@ -0,0 +1,15 @@
+// The DOM (Document Object Model) is a programming interface for web documents. It represents the structure of an
+// HTML or XML document as a tree-like model, here each element in the document is a node in the tree. 
+// The DOM tree starts with the root node, which represents the entire HTML document. 
+// The root node has child nodes, which represent the HTML elements (e.g., <html>, <head>, <body>, etc.)
+// example
+// - Document (root node)
+//   - <html> (element node)
+//     - <head> (element node)
+//       - <title> (element node)
+//         - Text Node: "My Web Page"
+//     - <body> (element node)
+//       - <h1> (element node)
+//         - Text Node: "Hello, DOM!"
+//       - <p> (element node)
+//         - Text Node: "Welcome to my webpage."
