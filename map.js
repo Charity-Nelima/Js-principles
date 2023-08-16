23a4ebe77ced2b0204af70488d5100a98a958991
charity <wa.nelimacharity@gmail.com>
2023-08-16 21:27:32 +0300

    mapping in javascript

--- map.js
+++ map.js
@@ -1,2 +1,10 @@
 // In JavaScript, "mapping" typically refers to the process of transforming elements in an array using a provided function to produce a new array with the transformed elements. 
 // The map() method is a powerful tool for achieving this transformation. 
+// example
+const numbers = [1, 2, 3, 4, 5];
+
+const squaredNumbers = numbers.map(function(number) {
+  return number * number;
+});
+
+console.log(squaredNumbers);
