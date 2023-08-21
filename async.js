2615283700deca27cef05843a62909335aed157a
charity <wa.nelimacharity@gmail.com>
2023-08-21 18:05:05 +0300

    synchronous programming

---
+++ async.js
@@ -0,0 +1,8 @@
+// Synchronous Programming:
+// In synchronous programming, each operation is executed one after the other in a sequence. 
+// When an operation is being executed, 
+// the program "waits" for that operation to complete before moving on to the next one. 
+// example
+console.log('Start');
+console.log('Middle');
+console.log('End');
